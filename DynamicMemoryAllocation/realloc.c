93bca9664be16cee531198cbccd0f4e37745856c
rohitchoudhary1243 <rohitdudi7023@gmail.com>
2024-01-30 10:42:26 +0530

    reallocating the memory and input output

--- DynamicMemoryAllocation/realloc.c
+++ DynamicMemoryAllocation/realloc.c
@@ -6,19 +6,33 @@
 int main()
 {
     int *ptr;
-    ptr = (int *)calloc(5, sizeof(int));
-    for (int i = 0; i < 5; i++)
+    int n;
+    printf("enter the size of memory: ");
+    scanf("%d", &n);
+    ptr = (int *)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++)
     {
         printf("enter %d's student roll no.: ", i);
         scanf("%d", &ptr[i]);
-
+    }
+    for (int i = 0; i < n; i++)
+    {
         printf("%d's student roll no. is: %d\n", i, ptr[i]);
     }
-    ptr = realloc(ptr, 10);
-    for (int j = 0; j < 10; j++)
+
+    int m;
+    printf("update the size of memory: ");
+    scanf("%d", &m);
+
+    ptr = realloc(ptr, m); // parameters for reallocating
+    for (int j = 0; j < m; j++)
     {
         printf("enter %d's student phone: ", j);
         scanf("%d", &ptr[j]);
+    }
+    for (int j = 0; j < m; j++)
+    {
         printf("%d's student phone is: %d\n", j, ptr[j]);
     }
+    free(ptr);
 }
